e11b072dac904b6146118d601b96b5f79699e012
hussein18149 <husseinkatana17@gmail.com>
2020-01-31 16:35:54 +0300

    declared todocomponent in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TodoComponent } from './todo/todo.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
@@ -9,13 +10,15 @@ import {environment} from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
-    
+    TodoComponent
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireDatabaseModule,
     AngularFireModule.initializeApp(environment.firebase),
+    TodoComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
